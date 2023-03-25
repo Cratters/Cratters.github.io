8804b8f392bbe639071b7056f34c11f69f30fb3e
7R <aufinege0156@gmail.com>
2023-03-25 23:19:10 +0800

    瀑布流圖庫

--- js/script.js
+++ js/script.js
@@ -52,6 +52,7 @@ links.forEach(link => {
       .then(data => {
         const content = document.querySelector('#content');
         content.innerHTML = data;
+        if(page == 'art') loadImage();
       })
       .catch(error => {
         console.error(`Error loading ${page}.html`, error);
@@ -76,4 +77,53 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error('Failed to load default content', error);
       });
   });
-  
+
+function loadImage() {      
+  console.log('載入圖片');
+  const gallery = document.querySelector('#gallery');
+  const images = [];
+
+  for (let i = 1; i <= 19; i++) {
+    images.push({
+      src: `/images/art/${i}.png`,
+      caption: `Image ${i}`
+    });
+  }
+
+  // 生成圖片元素
+  function createImageElement(image) {
+    const img = document.createElement('img');
+    img.src = image.src;
+    img.className = "img img-fluid";
+    const a = document.createElement('a');
+    a.dataset.fancybox = 'gallery',
+    a.href = image.src,
+    a.appendChild(img);
+    const figure = document.createElement('div');
+    figure.className="col-sm-6 col-md-3 col-lg-2 col-xl-2 item box"
+    figure.appendChild(a);
+    gallery.appendChild(figure);
+  };
+
+  // 生成圖片
+  images.forEach(createImageElement);
+    
+  $(function(){
+    $('.masonry').masonry({
+        itemSelector: '.item'
+    });
+  });
+  $(function(){
+      var $container = $('.masonry');
+      $container.imagesLoaded(function(){
+          $container.masonry({
+          itemSelector: '.item'
+          })
+      });
+  });
+
+  // 使用 imagesLoaded 確保所有圖片都已載入
+  imagesLoaded(gallery, function() {
+    masonry.layout();
+  });
+}
